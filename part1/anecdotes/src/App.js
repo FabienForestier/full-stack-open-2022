dfaccb012613b36aacf98c4f7d52b948cd015f2f
Fabien FORESTIER <fab.forestier42@gmail.com>
2022-09-11 17:08:27 +0200

    Part 1 - 1.13*: anecdotes step2 - Add vote

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -13,7 +13,7 @@ const App = () => {
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
-  ]
+  ];
 
   const getRandomInt = (max) => {
     return Math.floor(Math.random() * max);
@@ -30,11 +30,20 @@ const App = () => {
     }
   }
 
+  const voteForCurrentAnecdote = () => {
+    const pointsCopy = [...points];
+    pointsCopy[selected] += 1;
+    setPoints(pointsCopy);
+  }
+
   const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(anecdotes.map(() => 0))
 
   return (
     <div>
       <p>{anecdotes[selected]}</p>
+      <p>has {points[selected]} votes</p>
+      <Button text="Vote" onClick={voteForCurrentAnecdote} />
       <Button text="Next anecdote" onClick={pickRandomAnecdote} />
     </div>
   )
