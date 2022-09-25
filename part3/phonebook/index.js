012cad370de4467e509d26dd02208124cb30020e
Fabien FORESTIER <fab.forestier42@gmail.com>
2022-09-25 18:31:48 +0200

    Part3 - 3.4: Phonebook backend step4 - Add delete endpoint

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -26,22 +26,29 @@ let persons = [
 
 app.get('/', (request, response) => {
     response.send('<h1>Phone Book API</h1>');
-})
+});
 
 app.get('/info', (request, response) => {
     response.send(`<p>Phone book has info for ${persons.length}</p><p>${new Date().toString()}</p>`);
-})
+});
 
 app.get('/api/persons', (request, response) => {
     response.json(persons);
-})
+});
 
 app.get('/api/persons/:id', (request, response) => {
     const personId = Number(request.params.id);
 
     const matchingPerson = persons.find((person) => person.id === personId);
     return matchingPerson ? response.json(matchingPerson) : response.status(404).end();
-})
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+    const personId = Number(request.params.id);
+
+    persons = persons.filter((person) => person.id !== personId);
+    return response.status(204).end();
+});
 
 const PORT = 3001
 app.listen(PORT, () => {
