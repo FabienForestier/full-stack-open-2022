a7242f131c26a215eab5194bc11198b2bc097bda
Fabien FORESTIER <fab.forestier42@gmail.com>
2022-09-28 20:35:39 +0200

    3.7: Phonebook backend step7 - Add morgan middleware

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
+
 app.use(express.json());
+app.use(morgan('tiny'));
 
 function generateId() {
     return Math.floor(Math.random() * 1000000);
