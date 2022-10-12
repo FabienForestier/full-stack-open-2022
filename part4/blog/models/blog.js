2145502e3395ba33ac91d68105f719c938f69878
Fabien FORESTIER <fab.forestier42@gmail.com>
2022-10-12 22:09:33 +0200

    4.2 Blog list, step2 - Refacto architecture

---
+++ part4/blog/models/blog.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+blogSchema.set('toJSON', {
+  transform: (_, blog) => {
+    blog.id = blog._id.toString()
+    delete blog._id
+    delete blog.__v
+  }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
