ea3e6da7762a49884b5b06cdf3070f6a66dec3d5
Fabien FORESTIER <fab.forestier42@gmail.com>
2023-05-17 11:12:11 +0200

    6.2: unicafe revisited, step2

---
+++ part6/unicafe-redux/src/index.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import { createStore } from 'redux'
+import reducer from './reducer'
+
+const store = createStore(reducer)
+
+const App = () => {
+  const incrementGood = () => {
+    store.dispatch({
+      type: 'GOOD'
+    })
+  }
+  const incrementOk = () => {
+    store.dispatch({
+      type: 'OK'
+    })
+  }
+  const incrementBad = () => {
+    store.dispatch({
+      type: 'BAD'
+    })
+  }
+  const resetCounters = () => {
+    store.dispatch({
+      type: 'ZERO'
+    })
+  }
+
+  return (
+    <div>
+      <button onClick={incrementGood}>good</button> 
+      <button onClick={incrementOk}>ok</button> 
+      <button onClick={incrementBad}>bad</button>
+      <button onClick={resetCounters}>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
