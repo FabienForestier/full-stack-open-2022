d075231895ee26bde6c745585c97339945112a10
Fabien FORESTIER <fab.forestier42@gmail.com>
2023-05-17 17:07:17 +0200

    6.9 Better anecdotes, step7

---
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -0,0 +1,15 @@
+const filterReducer = (state = '', action) => {
+  switch(action.type) {
+    case 'FILTER':
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+export const filterChange = (filterText) => ({
+  type: 'FILTER',
+  payload: filterText
+}) 
+
+export default filterReducer;
